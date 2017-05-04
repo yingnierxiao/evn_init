64e61e18cdfc460e9e96bd195616b14cdd44be45
zj <yingnierxiao@qq.com>
2017-05-04 19:49:48 +0800

    add 3rd

--- install_openresty.sh
+++ install_openresty.sh
@@ -16,3 +16,17 @@ chmod +x shadowsocks_install/shadowsocks-libev.sh
 
 chmod 500 ./kcp-server/install-kcp-server.sh
 ./kcp-server/install-kcp-server.sh install
+
+
+netstat -nutlp
+
+# tcp        0      0 127.0.0.1:12948         0.0.0.0:*               LISTEN      1118/socks5         
+# tcp        0      0 0.0.0.0:22              0.0.0.0:*               LISTEN      5041/sshd           
+# tcp        0      0 0.0.0.0:8388            0.0.0.0:*               LISTEN      873/ss-server       
+# tcp6       0      0 :::22                   :::*                    LISTEN      5041/sshd           
+# tcp6       0      0 :::8388                 :::*                    LISTEN      873/ss-server       
+# udp        0      0 0.0.0.0:8388            0.0.0.0:*                           873/ss-server       
+# udp        0      0 127.0.0.1:323           0.0.0.0:*                           5306/chronyd        
+# udp6       0      0 :::45678                :::*                                1126/kcp-server     
+# udp6       0      0 :::8388                 :::*                                873/ss-server       
+# udp6       0      0 ::1:323                 :::*                                5306/chronyd  
